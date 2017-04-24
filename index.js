4810a1d212814a4f1fd30e7c451f0292fb504281
Cary Tabares <ctab33@me.com>
2017-04-24 14:18:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var cart = []
+var cart = [];
+
 
 function getCart() {
  return cart
@@ -9,22 +10,45 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * (100 - 0)) + 0;
+
+    var cartContents = {[item]:price};
+    cart.push(cartContents);
+    console.log (`${item} has been added to your cart.`);
+      return cart;
 }
 
 function viewCart() {
-  // write your code here
+ itemAndPrice = cart [i];
+ var item;
+ item = Object.keys(itemAndPrice)[0];
+ var price;
+ price = itemAndPrice[item];
+  if (cart.length === 0) {
+    console.log ("Your shopping cart is empty.");
+  } else {
+    for (var i in cart) {
+      console.log (`In your cart, you have ${item} at ${price}.`);
+  }
+  }
 }
 
 function total() {
   // write your code here
+  let t = 0
+
+  for (var i = 0, l = cart.length; i < l; i++) {
+    for (var item in cart[i]) {
+      t += cart[i][item]
+    }
+  }
+  return t;
 }
 
 function removeFromCart(item) {
-  // write your code here
+
 }
 
 function placeOrder(cardNumber) {
   // write your code here
 }
-
